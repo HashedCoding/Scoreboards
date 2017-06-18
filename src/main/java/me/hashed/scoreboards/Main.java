99ab4d27cd2ed4e63fdd19bbb245a5f09f4e6742
Larsfx <info@larsfx.nl>
2017-06-18 20:39:34 +0200

    Uploaded code.

---
+++ src/main/java/me/hashed/scoreboards/Main.java
@@ -0,0 +1,8 @@
+package me.hashed.scoreboards;
+
+/**
+ * Created by Lars Kalkman on 18-6-2017.
+ */
+public class Main
+{
+}
---
+++ src/main/java/me/hashed/scoreboards/PlayerJoin.java
@@ -0,0 +1,7 @@
+package me.hashed.scoreboards;
+
+/**
+ * Created by Lars Kalkman on 18-6-2017.
+ */
+public class PlayerJoin {
+}
